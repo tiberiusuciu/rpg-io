edd5ef438135e58a1c19b4b7bf6753ceec462913
Tiberiu Cristian Suciu <tiberiu.cristian.suciu@gmail.com>
2017-06-01 02:41:56 -0400

    Basic page layout with coloration indicating spacing

--- app/components/MainPage.js
+++ app/components/MainPage.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
-// import styles from '../../../styles/layouts/drawMyRoof/Header.css';
+import TerminalBox from './TerminalBox';
+import DetailsBox from './DetailsBox';
+import CanvasBox from './CanvasBox';
+
+import styles from '../styles/MainPage.css';
 
 class MainPage extends Component {
 	render() {
 		return (
-			<div>
-				<h1>Test</h1>
+			<div className={styles.MainPageLayout}>
+				<div className={styles.TerminalLayout + " col-xs-8"}>
+					Test Terminal
+				</div>
+				<div className={styles.InformationLayout + " col-xs-4"}>
+					<div className={styles.CanvasLayout}>
+						Test Canvas
+					</div>
+					<div className={styles.DetailsBox}>
+						Test Details
+					</div>
+				</div>
 			</div>
 		)
 	}
